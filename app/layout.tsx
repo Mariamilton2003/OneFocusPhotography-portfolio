01dd87bbdab62f30a1340595383ef69bc805b581
Maria Milton <miltonmaria732@gmail.com>
2025-02-25 23:49:14 +0530

    bulid fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,49 +7,48 @@ import Footer from "@/components/Footer/Footer";
 const libre_bodoni = localFont({
   src: [
     {
-      path: 'fonts/libre-bodoni/Libre-Bodoni-master/fonts/TTF/LibreBodoni-Regular.ttf',
+      path: '../public/fonts/libre-bodoni/Libre-Bodoni-master/fonts/TTF/LibreBodoni-Regular.ttf',
       weight: '400'
     },
     {
-      path: 'fonts/libre-bodoni/Libre-Bodoni-master/fonts/TTF/LibreBodoni-Italic.ttf',
+      path: '../public/fonts/libre-bodoni/Libre-Bodoni-master/fonts/TTF/LibreBodoni-Italic.ttf',
       weight: '400',
       style: 'italic'
     },
     {
-      path: 'fonts/libre-bodoni/Libre-Bodoni-master/fonts/TTF/LibreBodoni-Bold.ttf',
+      path: '../public/fonts/libre-bodoni/Libre-Bodoni-master/fonts/TTF/LibreBodoni-Bold.ttf',
       weight: '700'
     },
     {
-      path: 'fonts/libre-bodoni/Libre-Bodoni-master/fonts/TTF/LibreBodoni-BoldItalic.ttf',
+      path: '../public/fonts/libre-bodoni/Libre-Bodoni-master/fonts/TTF/LibreBodoni-BoldItalic.ttf',
       weight: '700',
       style: 'italic'
     }
   ],
   variable: '--font-libre-bodoni'
 });
-
 const sourceSansPro = localFont({
   src: [
     {
-      path: 'fonts/source-sans-pro/SourceSansPro-Regular.ttf',
+      path: '../public/fonts/source-sans-pro/SourceSansPro-Regular.ttf',
       weight: '400'
     },
     {
-      path: 'fonts/source-sans-pro/SourceSansPro-Italic.ttf',
+      path: '../public/fonts/source-sans-pro/SourceSansPro-Italic.ttf',
       weight: '400',
       style: 'italic'
     },
     {
-      path: 'fonts/source-sans-pro/SourceSansPro-Bold.ttf',
+      path: '../public/fonts/source-sans-pro/SourceSansPro-Bold.ttf',
       weight: '700'
     },
     {
-      path: 'fonts/source-sans-pro/SourceSansPro-BoldItalic.ttf',
+      path: '../public/fonts/source-sans-pro/SourceSansPro-BoldItalic.ttf',
       weight: '700',
       style: 'italic'
     },
     {
-      path: 'fonts/source-sans-pro/SourceSansPro-SemiBold.ttf',
+      path: '../public/fonts/source-sans-pro/SourceSansPro-SemiBold.ttf',
       weight: '600'
     }
   ],
